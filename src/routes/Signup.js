bd2979e4fa9c445d7d6f5dc76364d19ac3e7d0f7
Marc Ferland <mferland@esentire.com>
2017-03-27 23:28:01 -0400

    - Added registration capabilities - Added company logo

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import classNames from 'classnames';
-import { Link, withRouter } from 'react-router';
+import React from 'react'
+import classNames from 'classnames'
+import { Link, withRouter } from 'react-router'
+import Cookie from 'react-cookie'
+import Axios from 'axios'
+import querystring from 'querystring'
+import Auth from '../common/auth'
 
 import {
   Row,
@@ -19,10 +23,32 @@ import {
   ButtonGroup,
   ButtonToolbar,
   PanelContainer,
-} from '@sketchpixy/rubix';
+} from '@sketchpixy/rubix'
 
 @withRouter
 export default class Signup extends React.Component {
+
+  constructor(props, context) {
+    super(props)
+
+    // user is already logged in (token is verified)
+    // redirect them to dashboard
+    if ( Auth.isUserAuthenticated() ) this.props.router.replace(::this.getPath('dashboard'))
+
+    this.state = {
+      errors: {},
+      user: {
+        name: '',
+        email: '',
+        password: '',
+        role: ''
+      }
+    };
+
+    this.processForm = this.processForm.bind(this)
+    this.changeUser = this.changeUser.bind(this)
+  }
+
   back(e) {
     e.preventDefault();
     e.stopPropagation();
@@ -37,6 +63,65 @@ export default class Signup extends React.Component {
     $('html').removeClass('authentication');
   }
 
+  processForm(e) {
+    e.preventDefault()
+
+    const name = this.state.user.name
+    const email = this.state.user.email
+    const password = this.state.user.password
+    const role = 'esadmin'
+    const date = new Date()
+    const config = {
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded'
+      }
+    }
+
+    Axios
+      .post('http://localhost:3001/auth/signup', querystring.stringify({
+        name: name,
+        email: email,
+        password: password,
+        role: role
+      }), config)
+      .then(response => {
+        console.log(response.data)
+        if (response.data.token) {
+          date.setMinutes(date.getMinutes() + 15)
+
+          Cookie.save('token', response.data.token, {
+            domain: 'localhost',
+            expires: date,
+            maxAge: 900,
+            path: '/',
+            secure: false
+          })
+          this.props.router.push(::this.getPath('dashboard'))
+        }
+        else {
+          // ...
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  /**
+   * Change the user object.
+   *
+   * @param {object} event - the JavaScript event object
+   */
+  changeUser(event) {
+    const field = event.target.name
+    const user = this.state.user
+    user[field] = event.target.value
+
+    this.setState({
+      user
+    })
+  }
+
   getPath(path) {
     var dir = this.props.location.pathname.search('rtl') !== -1 ? 'rtl' : 'ltr';
     path = `/${dir}/${path}`;
@@ -45,7 +130,7 @@ export default class Signup extends React.Component {
 
   render() {
     return (
-      <div id='auth-container' className='login'>
+      <div id='auth-container' className='login' style={{backgroundImage: "url(/imgs/common/background.jpg)"}}>
         <div id='auth-row'>
           <div id='auth-cell'>
             <Grid>
@@ -54,18 +139,20 @@ export default class Signup extends React.Component {
                   <PanelContainer controls={false}>
                     <Panel>
                       <PanelBody style={{padding: 0}}>
-                        <div className='text-center bg-darkblue fg-white'>
-                          <h3 style={{margin: 0, padding: 25}}>Sign up</h3>
+                        <div className='text-center bg-esentire fg-white'>
+                          <h3 style={{margin: 0, padding: 25}}>
+                            <img src='/imgs/common/esentire-logo-white.png' height='40' />
+                          </h3>
                         </div>
                         <div>
                           <div style={{padding: 25, paddingTop: 0, paddingBottom: 0, margin: 'auto', marginBottom: 25, marginTop: 25}}>
-                            <Form onSubmit={::this.back}>
-                              <FormGroup controlId='username'>
+                            <Form onSubmit={::this.processForm}>
+                              <FormGroup controlId='name'>
                                 <InputGroup bsSize='large'>
                                   <InputGroup.Addon>
                                     <Icon glyph='icon-fontello-user' />
                                   </InputGroup.Addon>
-                                  <FormControl autoFocus type='text' className='border-focus-blue' placeholder='Username' />
+                                  <FormControl name='name' onChange={::this.changeUser} autoFocus type='text' className='border-focus-blue' placeholder='Name' />
                                 </InputGroup>
                               </FormGroup>
                               <FormGroup controlId='emailaddress'>
@@ -73,7 +160,7 @@ export default class Signup extends React.Component {
                                   <InputGroup.Addon>
                                     <Icon glyph='icon-fontello-mail' />
                                   </InputGroup.Addon>
-                                  <FormControl type='email' className='border-focus-blue' placeholder='support@sketchpixy.com' />
+                                  <FormControl name='email' onChange={::this.changeUser} type='email' className='border-focus-blue' placeholder='email@address.com' />
                                 </InputGroup>
                               </FormGroup>
                               <FormGroup controlId='password'>
@@ -81,14 +168,14 @@ export default class Signup extends React.Component {
                                   <InputGroup.Addon>
                                     <Icon glyph='icon-fontello-key' />
                                   </InputGroup.Addon>
-                                  <FormControl type='password' className='border-focus-blue' placeholder='password' />
+                                  <FormControl name='password' onChange={::this.changeUser} type='password' className='border-focus-blue' placeholder='password' />
                                 </InputGroup>
                               </FormGroup>
                               <FormGroup>
                                 <Grid>
                                   <Row>
                                     <Col xs={12} collapseLeft collapseRight>
-                                      <Button type='submit' outlined lg bsStyle='blue' block onClick={::this.back}>Create account</Button>
+                                      <Button type='submit' outlined lg bsStyle='blue' block>Create account</Button>
                                     </Col>
                                   </Row>
                                 </Grid>
