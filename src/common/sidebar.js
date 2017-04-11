b65912b592b9a27489761742ad858a89c5ea217d
Marc Ferland <mferland@esentire.com>
2017-04-11 18:45:30 -0400

    Removed Reports from sidebar

--- src/common/sidebar.js
+++ src/common/sidebar.js
@@ -48,7 +48,6 @@ class ApplicationSidebar extends React.Component {
                   <div className='sidebar-header'>PAGES</div>
 
                   <SidebarNavItem glyph='icon-fontello-gauge' name='Dashboard' href={::this.getPath('dashboard')} />
-                  <SidebarNavItem glyph='icon-fontello-doc-text-inv' name='Reports' href={::this.getPath('reports')} />
                   <SidebarNavItem glyph='icon-fontello-user-1' name='Profile' href={::this.getPath('social')} />
                   <SidebarNavItem glyph='icon-fontello-cog-1' name={<span>Settings <Label className='bg-darkgreen45 fg-white'>1</Label></span>}>
                     <SidebarNav>
